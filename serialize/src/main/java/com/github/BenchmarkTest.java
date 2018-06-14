6ca994abe27809dfd581a903d2ab294f44cbdef5
benjaminwhx <benjaminwhx@sina.com>
2018-06-15 00:34:23 +0800

    init serialize

--- serialize/src/main/java/com/github/BenchmarkTest.java
+++ serialize/src/main/java/com/github/BenchmarkTest.java
@@ -11,65 +11,51 @@ import com.github.bean.protobuf.InnerMessageProtos;
 import com.github.bean.protobuf.MessageProtos;
 import com.github.serializer.*;
 import com.google.protobuf.ByteString;
+import org.openjdk.jmh.annotations.*;
 
-import java.lang.management.ManagementFactory;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * User: benjamin.wuhaixu
- * Date: 2018-06-11
- * Time: 5:42 pm
+ * Date: 2018-06-12
+ * Time: 9:19 pm
  */
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 5, timeUnit = TimeUnit.SECONDS)
+@Fork(3)
+@Threads(8)
 public class BenchmarkTest {
 
-	private static void testTemplate(ISerializer serializer, Object obj, int count, String name) {
-		// 先进行预热，加载一些类，避免影响测试
-		for (int i = 0; i < 10; i++) {
-			byte[] bytes = serializer.serialize(obj);
-			serializer.deserialize(bytes, obj.getClass());
-		}
-		restoreJvm(); // 进行GC回收
-		// 进行测试
-		long start = System.nanoTime();
-		long size = 0l;
-		for (int i = 0; i < count; i++) {
-			byte[] bytes = serializer.serialize(obj);
-			size = size + bytes.length;
-			serializer.deserialize(bytes, obj.getClass());
-			bytes = null;
-		}
-		long nsCost = (System.nanoTime() - start);
-		System.out.println(name + " total cost=" + nsCost + "ns(" + nsCost/1000000 + "ms)" + " , each cost="
-				+ nsCost / count + "ns , and byte sizes = " + size / count);
-		restoreJvm();// 每次测试模板调用完成后system.gc保证下一轮的功能测试
-	}
+	private ISerializer javaSerializer = new DefaultJavaSerializer();
+	private ISerializer xStreamSerializer = new XmlSerializer();
+	private ISerializer javaXmlSerializer = new Xml2Serializer();
+	private ISerializer fastJsonSerializer = new FastJsonSerializer();
+	private ISerializer hessianSerializer = new HessianSerializer();
+	private ISerializer protobufSerializer = new ProtobufSerializer();
+	private ISerializer protostuffSerializer = new ProtostuffSerializer();
+	private ISerializer avroSerializer = new AvroSerializer();
+	private ISerializer marshallingSerializer = new MarshallingSerializer();
+	private MessageProtos.Message probufBean = getProtobufBean();
+	private MessageAvro avroBean = getAvroBean();
+	private MessageObj pojoBean = getPojoBean();
 
-	private static void restoreJvm() {
-		int maxRestoreJvmLoops = 10;
-		long memUsedPrev = memoryUsed();
-		for (int i = 0; i < maxRestoreJvmLoops; i++) {
-			System.runFinalization();
-			System.gc();
+	byte[] javaSerializeBytes = javaSerializer.serialize(pojoBean);
+	byte[] xStreamSerializeBytes = xStreamSerializer.serialize(pojoBean);
+	byte[] javaXmlSerializeBytes = javaXmlSerializer.serialize(pojoBean);
+	byte[] fastJsonSerializeBytes = fastJsonSerializer.serialize(pojoBean);
+	byte[] hessianSerializeBytes = hessianSerializer.serialize(pojoBean);
+	byte[] protobufSerializeBytes = protobufSerializer.serialize(probufBean);
+	byte[] protostuffSerializeBytes = protostuffSerializer.serialize(pojoBean);
+	byte[] avroSerializeBytes = avroSerializer.serialize(avroBean);
+	byte[] marshallingSerializeBytes = marshallingSerializer.serialize(pojoBean);
 
-			long memUsedNow = memoryUsed();
-			// break early if have no more finalization and get constant mem used
-			if ((ManagementFactory.getMemoryMXBean().getObjectPendingFinalizationCount() == 0)
-					&& (memUsedNow >= memUsedPrev)) {
-				break;
-			} else {
-				memUsedPrev = memUsedNow;
-			}
-		}
-	}
-
-	private static long memoryUsed() {
-		Runtime rt = Runtime.getRuntime();
-		return rt.totalMemory() - rt.freeMemory();
-	}
-
-	private static MessageProtos.Message getProtobufBean() {
+	private MessageProtos.Message getProtobufBean() {
 		MessageProtos.Message.Builder messageBuilder = MessageProtos.Message.newBuilder();
 
 		messageBuilder.setStrObj("message");
@@ -91,7 +77,7 @@ public class BenchmarkTest {
 		return messageBuilder.build();
 	}
 
-	private static MessageAvro getAvroBean() {
+	private MessageAvro getAvroBean() {
 		MessageAvro messageAvro = new MessageAvro();
 		messageAvro.setStrObj("message");
 		messageAvro.setFloatObj(1f);
@@ -111,7 +97,7 @@ public class BenchmarkTest {
 		return messageAvro;
 	}
 
-	private static MessageObj getPojoBean() {
+	private MessageObj getPojoBean() {
 		InnerMessageObj innerMessageObj = new InnerMessageObj();
 		innerMessageObj.setId(1);
 		innerMessageObj.setName("inner");
@@ -132,27 +118,84 @@ public class BenchmarkTest {
 		return msg;
 	}
 
-	public static void main(String[] args) {
-		MessageProtos.Message protobufBean = getProtobufBean();
-		MessageAvro avroBean = getAvroBean();
-		MessageObj pojoBean = getPojoBean();
-		final int testCount = 1000 * 500;
-		ISerializer defaultJavaSerializer = new DefaultJavaSerializer();
-		ISerializer xmlSerializer = new XmlSerializer();
-		ISerializer jsonSerializer = new FastJsonSerializer();
-		ISerializer hessianSerializer = new HessianSerializer();
-		ISerializer protobufSerializer = new ProtobufSerializer();
-		ISerializer protostuffSerializer = new ProtostuffSerializer();
-		ISerializer avroSerializer = new AvroSerializer();
-		ISerializer marshallingSerializer = new MarshallingSerializer();
+	@Benchmark
+	public byte[] testDefaultJavaSerialize() {
+		return javaSerializer.serialize(pojoBean);
+	}
+	@Benchmark
+	public MessageObj testDefaultJavaDeSerialize() {
+		return javaSerializer.deserialize(javaSerializeBytes, MessageObj.class);
+	}
 
-		testTemplate(defaultJavaSerializer, pojoBean, testCount, "JDK-Serializer");
-		testTemplate(xmlSerializer, pojoBean, testCount, "XStream");
-		testTemplate(jsonSerializer, pojoBean, testCount, "FastJson");
-		testTemplate(hessianSerializer, pojoBean, testCount, "Hessian");
-		testTemplate(protobufSerializer, protobufBean, testCount, "Protobuf");
-		testTemplate(protostuffSerializer, pojoBean, testCount, "Protostuff");
-		testTemplate(avroSerializer, avroBean, testCount, "Avro");
-		testTemplate(marshallingSerializer, pojoBean, testCount, "JBoss-Marshalling");
+	@Benchmark
+	public byte[] testXStreamSerialize() {
+		return xStreamSerializer.serialize(pojoBean);
+	}
+	@Benchmark
+	public MessageObj testXStreamDeSerialize() {
+		return xStreamSerializer.deserialize(xStreamSerializeBytes, MessageObj.class);
+	}
+
+	@Benchmark
+	public byte[] testJavaXmlSerialize() {
+		return javaXmlSerializer.serialize(pojoBean);
+	}
+	@Benchmark
+	public MessageObj testJavaXmlDeSerialize() {
+		return javaXmlSerializer.deserialize(javaXmlSerializeBytes, MessageObj.class);
+	}
+
+	@Benchmark
+	public byte[] testFastJsonSerialize() {
+		return fastJsonSerializer.serialize(pojoBean);
+	}
+	@Benchmark
+	public MessageObj testFastJsonDeSerialize() {
+		return fastJsonSerializer.deserialize(fastJsonSerializeBytes, MessageObj.class);
+	}
+
+	@Benchmark
+	public byte[] testHessianSerialize() {
+		return hessianSerializer.serialize(pojoBean);
+	}
+	@Benchmark
+	public MessageObj testHessianDeSerialize() {
+		return hessianSerializer.deserialize(hessianSerializeBytes, MessageObj.class);
+	}
+
+	@Benchmark
+	public byte[] testProtobufSerialize() {
+		return protobufSerializer.serialize(probufBean);
+	}
+	@Benchmark
+	public MessageProtos.Message testProtobufDeSerialize() {
+		return protobufSerializer.deserialize(protobufSerializeBytes, MessageProtos.Message.class);
+	}
+
+	@Benchmark
+	public byte[] testProtostuffSerialize() {
+		return protostuffSerializer.serialize(pojoBean);
+	}
+	@Benchmark
+	public MessageObj testProtostuffDeSerialize() {
+		return protostuffSerializer.deserialize(protostuffSerializeBytes, MessageObj.class);
+	}
+
+	@Benchmark
+	public byte[] testAvroSerialize() {
+		return avroSerializer.serialize(avroBean);
+	}
+	@Benchmark
+	public MessageAvro testAvroDeSerialize() {
+		return avroSerializer.deserialize(avroSerializeBytes, MessageAvro.class);
+	}
+
+	@Benchmark
+	public byte[] testMarshallingSerialize() {
+		return marshallingSerializer.serialize(pojoBean);
+	}
+	@Benchmark
+	public MessageObj testMarshallingDeSerialize() {
+		return marshallingSerializer.deserialize(marshallingSerializeBytes, MessageObj.class);
 	}
 }
